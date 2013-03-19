c5c9cbe702fd1a3971a7534d566fc113adeb2caa
jonathanhadams <jonathanhadams@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-19 15:21:31 +0000

    upload
    git-svn-id: https://plugins.svn.wordpress.org/grid-products/trunk@684264 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- grid-products.php
+++ grid-products.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Grid Products
-Description: Manage product listings
+Plugin Name: grid Products
+Description: Manage product products
 Author: Evan M
 Author URI: website-design-firm.com
 Plugin URI: website-design-firm.com
-Version: 1
+Version: 1.1
 */ 
 
 
@@ -15,11 +15,13 @@ add_image_size('product-image2',195,140, true);
 
 /*
 
-[product cat="liftmaster" hidetitle="yes" featured="yes" view="list" buttontext="View Gallery" des="no" maxdes="25"]
+[product cat="liftmaster" hidetitle="yes" featured="yes" view="list" buttontext="View Gallery"]
 
 */
 
 
+
+include 'metaboxes.php';
 
 
 add_action( 'init', 'create_product_post_types' );
@@ -35,7 +37,35 @@ function create_product_post_types() {
 		'update_item' => __( 'Update Product Category' ),
 		'add_new_item' => __( 'Add New Product Category' ),
 		'new_item_name' => __( 'New Product Category Name' ),
+	
   ); 	
+  
+  
+  
+add_filter('post_updated_messages', 'product_updated_messages');
+function product_updated_messages( $messages ) {
+
+$messages['grid_products'] = array(
+0 => '', // Unused. Messages start at index 1.
+1 => sprintf( __('Product updated. <a href="%s">View Product</a>'), esc_url( get_permalink($post_ID) ) ),
+2 => __('Custom field updated.'),
+3 => __('Custom field deleted.'),
+4 => __('Product updated.'),
+/* translators: %s: date and time of the revision */
+5 => isset($_GET['revision']) ? sprintf( __('Product restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+6 => sprintf( __('Product published. <a href="%s">View Product</a>'), esc_url( get_permalink($post_ID) ) ),
+7 => __('Contact saved.'),
+8 => sprintf( __('Product submitted. <a target="_blank" href="%s">Preview Product</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+9 => sprintf( __('Product scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview Product</a>'),
+  // translators: Publish box date format, see http://php.net/date
+  date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+10 => sprintf( __('Product draft updated. <a target="_blank" href="%s">Preview Product</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+);
+
+return $messages;
+}
+   
+  
   	register_taxonomy('grid_product_category',array('grid_products'), array(
 		'hierarchical' => true,
 		'labels' => $labels,
@@ -48,10 +78,16 @@ function create_product_post_types() {
 			'labels' => array(
 				'name' => __( 'Products' ),
 				'singular_name' => __( 'Product' ),
+				 'add_new' => _x('Add New Product', 'Product'),
+    'add_new_item' => __('Add New Product'),
 				'edit_item'	=>	__( 'Edit Product'),
+				 'view_item' => __('View Product'),
 				'add_new_item'	=>	__( 'Add Product')
+
 			),
 			'public' => true,
+			'menu_position' => 10,
+			'menu_icon' => plugins_url( 'images/product_edit.png', __FILE__ ),
 			'show_ui' => true,
 			'capability_type' => 'post',
 			'rewrite' => array( 'slug' => 'product', 'with_front' => false ),
@@ -60,20 +96,18 @@ function create_product_post_types() {
 			 'title',
 	  'editor',
 	  'excerpt',
-	  'trackbacks',
-	  'custom-fields',
 	  'revisions',
 	  'thumbnail',
+	  'custom-fields',
 	  'author',
-	  'page-attributes',
 	  )
 		)
 	);
 }	
 
 
-add_action('restrict_manage_posts','restrict_listings_by_categories');
-function restrict_listings_by_categories() {
+add_action('restrict_manage_posts','restrict_products_by_categories');
+function restrict_products_by_categories() {
     global $typenow;
     global $wp_query;
     if ($typenow=='grid_products') {
@@ -133,6 +167,7 @@ function product_shortcode($atts) {
 			} 
 					
 			global $wpdb; $catname = $wpdb->get_var("SELECT name FROM $wpdb->terms WHERE slug = '$cat'");
+			$countproducts='0';
 			$product_shortcode = '';
 			
 			if ( !empty( $cat ) && $hidetitle != 'yes' ) { $product_shortcode .= '<div class="product-catname">' . $catname . '</div>'; }	
@@ -156,17 +191,7 @@ function product_shortcode($atts) {
 			}
 			
 			
-			
-			/*
-			if ($view != 'list' && $featured != 'yes'){
-			$product_shortcode .= '<div class="productcategoryholder">';
-			}
-			
-			if ($view != 'list' && $featured == 'yes'){
-			$product_shortcode .= '<div class="productcategoryholder"  style="background: #eee;">';
-			}
-			*/
-			
+		
 			
 				if ($view != 'list' && $featured != 'yes'){
 			$product_shortcode .= '<ul class="products">';
@@ -177,29 +202,48 @@ function product_shortcode($atts) {
 			}
 			
 			
-			
-			
+						
 			
 			while($grid_products->have_posts()): $grid_products->the_post();
+			$countproducts++;
+			
+			$price=get_post_meta( get_the_ID(), '_grid_product_price', true );
 			
 		
 			
 			if ($view == 'list'){
-				$theimage=wp_get_attachment_image_src( get_post_thumbnail_id($post->ID) , 'product-image');
+				$theimage=wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()) , 'product-image');
 	
 				$product_shortcode .= '<tr><td align="center"><a href="' . get_permalink() . '"><img src="'.$theimage[0].'" alt="" /></a></td>';
 				$product_shortcode .= '<td><div class="product-title"><a href="' . get_permalink() . '">'. get_the_title().'</a></div>';
 				
+				
+				
+				
 				if ($buttontext == NULL){
-				$product_shortcode .= '<div class="product-excerpt">'.get_the_excerpt() . '</div></td></tr><tr><td colspan="2"><div class="productmoretag"><a href="' . get_permalink() . '">Read More</a></div></td></tr>';
+				$product_shortcode .= '<div class="product-excerpt"><p>'.get_the_excerpt().'</p>';
+				if($price != NULL){
+				$product_shortcode .= '<p><b>Price $'.$price.'</b></p>';
+				}
+				$product_shortcode .= '</div></td></tr>';
+				$product_shortcode .= '<tr><td colspan="2"><div class="productmoretag"><a href="' . get_permalink() . '">View Product</a></div></td></tr>';
+								
+				
 	            }else{
-	           $product_shortcode .= '<div class="product-excerpt">'.get_the_excerpt() . '</div></td></tr><tr><td colspan="2"><div class="productmoretag"><a href="' . get_permalink() . '">'.$buttontext.'</a></div></td></tr>';
+	           $product_shortcode .= '<div class="product-excerpt"><p>'.get_the_excerpt().'</p>';
+	           	if($price != NULL){
+				$product_shortcode .= '<p><b>Price $'.$price.'</b></p>';
+				}
+	           $product_shortcode .= '</div></td></tr>';
+	           $product_shortcode .= '<tr><td colspan="2"><div class="productmoretag"><a href="' . get_permalink() . '">'.$buttontext.'</a></div></td></tr>';
 	            }
+	            	
+
 	$product_shortcode .= '<tr><td colspan="2"><div class="spacer"></div></td></tr>';
 	
 	}else {
 	
-	$theimage=wp_get_attachment_image_src( get_post_thumbnail_id($post->ID) , 'product-image2');
+	$theimage=wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()) , 'product-image2');
 		
 	$product_shortcode .= '<li>';
 	$product_shortcode .= '<a href="' . get_permalink() . '"><img src="'.$theimage[0].'" style="max-width:195px;" alt="" />';
@@ -217,9 +261,20 @@ function product_shortcode($atts) {
  	$paragraph=implode (' ', $paragraph);
 	$product_shortcode .= '<p>' .$paragraph. '...</p>';
 	} 
+	
+	
+	
+
+	
+	if($price != NULL){
+	$product_shortcode .= '<p><b>Price $'.$price.'</b></p>';
+	}
+	
 	$product_shortcode .= '</a>';
+
+	
 	if ($buttontext == NULL){
-		$product_shortcode .= '<div class="productmoreholder"><div class="productmoretag"><a href="' . get_permalink() . '">Read More</a></div></div>';
+		$product_shortcode .= '<div class="productmoreholder"><div class="productmoretag"><a href="' . get_permalink() . '">View Product</a></div></div>';
     }else{
         $product_shortcode .= '<div class="productmoreholder"><div class="productmoretag"><a href="' . get_permalink() . '">'.$buttontext.'</a></div></div>';
     }		
@@ -233,23 +288,20 @@ function product_shortcode($atts) {
 
 	
 			endwhile; // end slideshow loop
-			
+				
 			
 		if ($view == 'list'){	
 	$product_shortcode .= '</tbody></table>';
 }	
 		
-		/*
-			if ($view != 'list'){
-			$product_shortcode .= '</div>';
-			}		
-		*/
 		
 			if ($view != 'list'){
 			$product_shortcode .= '</ul>';
 			}		
 			
-			
+if ($countproducts == '0') {
+echo 'No products are currently posted in this category';
+}			
 			
 			wp_reset_query();
 	
@@ -306,7 +358,7 @@ add_action('admin_menu', 'add_grid_product_option_page');
 
 function add_grid_product_option_page() {
 	// hook in the options page function
-	add_options_page('Grid Products', 'Grid Products', 'manage_options', __FILE__, 'grid_product_options_page');
+	add_options_page('grid Product', 'grid Product', 'manage_options', __FILE__, 'grid_product_options_page');
 
 }
 
@@ -315,17 +367,21 @@ function add_grid_product_option_page() {
 function grid_product_options_page() { 	
 	?>
 	<div class="wrap" style="width:500px">
-		<h2>Grid Products Shortcodes Explained</h2>
+		<h2>grid Products Shortcodes Explained</h2>
 		
-		<h3>Possible Shortcodes</h3>
+		<h3>Shortcode - [product]</h3>
+<h4>Full Shortcode With All Options Enabled :<br/><br/> [product cat="category-slug" hidetitle="yes" featured="yes" view="list"  buttontext="your text here"  des="no" maxdes="50"] <h4>
+		<h4>Below are the product shortcode options explained in detail 
+		<h4><font color="#FF0000">***Note ALL shortcode options are optional:</font><h4>
 		<ul>
 		<li><hr><h3>cat</h3> Used to display only produces in a certain category. If not set ALL products from any category will be shown.<br/><br/><b>Usage :</b> cat="category-slug"<br/><br/></li>
-		<li><hr><h3>id</h3> Used  to display a single product. <br/><b>* Note: </b>the cat & the id attributes are mutually exclusive. Don't use both in the same shortcode. <br/><br/><b>Usage :</b> id="1234" - where 1234 is the post ID.<br/><br/></li>
+		<li><hr><h3>id</h3> Used  to display a single product. <br/><b>* Note: </b>the cat & the id attributes are mutually exclusive. Don't use both in the same shortcode.
+<br/><br/><b>Usage :</b> id="1234" - where 1234 is the post ID.<br/><br/></li>
 		<li><hr><h3>hidetitle</h3> Used in conjunction with the "cat" shortcode to hide the category title incase you would like to use something else instead of the category name.<br/><br/><b>Usage :</b> hidetitle="yes"<br/><br/></li>
 		<li><hr><h3>featured</h3> Will set the background of the container to a default light grey.<br/><br/><b>Usage :</b> featured="yes"<br/><br/></li>
 		<li><hr><h3>view</h3> The default view is a grid view, if you would prefer to use "list" view set this 
 		attribute to equal list <br/><br/><b>Usage :</b> view="list"<br/><br/></li>
-		<li><hr><h3>buttontext</h3>The default button text is "Read More" if you would like to change the text use this attribute <br/><br/><b>Usage :</b> buttontext="your text here"<br/><br/></li>
+		<li><hr><h3>buttontext</h3>The default button text is "View Product" if you would like to change the text use this attribute <br/><br/><b>Usage :</b> buttontext="your text here"<br/><br/></li>
 		<li><hr><h3>des</h3> Used to disable the product excerpt in the default grid view. <br/><br/><b>Usage :</b> des="no"<br/><br/></li>
 		<li><hr><h3>maxdes</h3>  Used to set the number of words used in the 
 		excerpt in the default grid view. (default - 20) must be a number.<br/><br/><b>Usage :</b> maxdes="50"<br/><br/><hr></li>
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: jonathanhadams, ModDish
 Donate link: http://printingpeach.ca/
 Tags: grid products, products grid, products in table, products display
 Requires at least: 3.0.1
-Tested up to: 3.5
-Stable tag: 1.0
+Tested up to: 3.5.1
+Stable tag: 1.1
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -28,7 +28,9 @@ Extract the zip file and upload the contents to the wp-content/plugins/ director
 The plugin registers a new custom post type, so you'll want to update your permalinks. No need to change your permalink structure, just go to "Settings->Permalinks" and click "Save Changes" without making any modifications.
 
 
-Possible Shortcodes
+Possible Shortcode [product]
+
+Shortcode options :
 
 -------------------------------------------------------------
 
